ba3d55943af669267fa3946881bacfd7bbb5d358
OscarS4nchez <17470008@campeche.tecnm.mx>
2021-11-15 02:12:04 -0600

    Agrega la clase contador y realiza el conteo utilizando una instancia de esta clase.

--- ContadorManual/ContadorManual/MainPage.xaml.cs
+++ ContadorManual/ContadorManual/MainPage.xaml.cs
@@ -6,74 +6,29 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using ContadorManual.Model;
 namespace ContadorManual
 {
     public partial class MainPage : ContentPage
     {
-
-
-        private int _Conteo;
+        private Contador _Contador;
         public MainPage()
         {
-
-
-
-
-
-
-
-
-
-
-
-
             InitializeComponent();
-
-
-
-
-            _Conteo = 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            _Contador = new Contador();
+            ConteoLabel.Text = _Contador.Conteo.ToString();
         }
-
         private void ContarButton_Clicked(object sender, EventArgs e)
         {
-
-
-            _Conteo++;
-            ConteoLabel.Text = _Conteo.ToString();
-
-
+            _Contador.Contar();
+            ConteoLabel.Text = _Contador.Conteo.ToString();
         }
-
         private void ReiniciarButton_Clicked(object sender, EventArgs e)
         {
-
-
-
-            _Conteo = 0;
-            ConteoLabel.Text = _Conteo.ToString();
-
-
-
-
-
-
+            _Contador.ReiniciarConteo();
+            ConteoLabel.Text = _Contador.Conteo.ToString();
         }
     }
 }
+
+
